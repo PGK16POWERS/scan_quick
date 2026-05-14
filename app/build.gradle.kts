a406c6540ee15029cba9993ad8a5244c9b8e1c4f
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2026-05-14 10:58:28 +0200

    yosemite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/scan_quick/MainActivity.kt
+++ app/src/main/java/com/example/scan_quick/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.scan_quick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
 }
